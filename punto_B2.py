5eea6e4d34c758097b43b4a66343d9ae25705fff
DanielRquiroga <danielr.quirogac@ecci.edu.co>
2024-02-17 15:05:00 -0500

    correccion b2

--- punto_B2.py
+++ punto_B2.py
@@ -1,8 +1,8 @@
 import numpy as np
 #Realice un programa que calcule X números aleatorios en un rango determinado por el usuario.
-numeros = int(input('Ingrse la cantidad de numero que desea: /n'))
-inferior = int(input('ingrese rango inferior\n'))
-superior = int(input('ingrese rango isuperior\n'))
+numeros = int(input(f'ingrse cantidad de numeros a generar\n'))
+inferior = int(input(f'''ingrese rango inferior\n'''))
+superior = int(input(f'ingrese rango isuperior\n'))
 
 numero_entero_aleatorio = np.random.randint(inferior,superior + 1, size=(numeros))
-print(f'los numeros son {numero_entero_aleatorio}')
+print(f'''los numeros son {numero_entero_aleatorio}''')
